5dff87f69ed39a714daf54720df921eccef01f38
selleffinghamcounty <selleffinghamcounty@gmail.com>
2026-04-07 21:15:48 -0500

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Brian Sells Effingham",
+  description: "Seller landing page",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
